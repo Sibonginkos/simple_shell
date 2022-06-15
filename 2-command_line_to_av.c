cba90d2e009bc371f2c6242a9d024f51eca547bf
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-06-15 10:27:03 +0200

    Command line to av

---
+++ 2-command_line_to_av.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char string[50] = "Hello! We are learning about strtok";
+
+	// Extract the first token
+	char * token = strtok(string, " ");
+	// loop through the string to extract all other tokens
+	while ( token != NULL )
+	{
+		printf( " %s\n", token ); //printing each token
+		token = strtok(NULL, " ");
+	}
+	return (0);
+}
