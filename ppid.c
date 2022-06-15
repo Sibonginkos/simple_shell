3ca11bde21a546fa08165e6f1d07383bf68ea6b1
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-06-15 15:10:11 +0200

    Prints the environment and prompt

--- ppid.c
+++ ppid.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-     unsigned int process_id, p_process_id;
+	unsigned int process_id, p_process_id;
 
-    process_id = getpid();
-    p_process_id = getppid();
-    printf("%d\n", process_id);
-    printf("%d\n", p_process_id);
-    /* echo $$ print ppid */
-    return (0);
+	process_id = getpid();
+	p_process_id = getppid();
+	printf("%d\n", process_id);
+	printf("%d\n", p_process_id);
+	/* echo $$ print ppid */
+	return (0);
 }
