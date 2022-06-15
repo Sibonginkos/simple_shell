861d5be353c5a70f20717f9b210ab0ab7d58aba0
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-06-15 11:51:14 +0200

    Prints the PID of the parent process

---
+++ ppid.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - PPID
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+     unsigned int process_id, p_process_id;
+
+    process_id = getpid();
+    p_process_id = getppid();
+    printf("%d\n", process_id);
+    printf("%d\n", p_process_id);
+    /* echo $$ print ppid */
+    return (0);
+}
