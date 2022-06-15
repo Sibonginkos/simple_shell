d0638dd01e36756129e27e998c1f48e0e0f06c4e
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-06-15 11:35:42 +0200

    Gets n environment variable

---
+++ getenv.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main (void)
+{
+	printf("PATH : %s\n", getenv("PATH"));
+	printf("HOME : %s\n", getenv("HOME"));
+	printf("ROOT : %s\n", getenv("ROOT"));
+
+	return(0);
+}
