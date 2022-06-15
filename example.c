4a30142d6a233097c15c96dead31f364d7279dc5
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-06-15 11:30:32 +0200

    Example

---
+++ example.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * input -
+ * main -
+ * @s:
+ * @int:
+ * Return:
+ */
+
+int input (char * s, int length);
+int main ()
+{
+	char *buffer;
+	size_t bufsize = 32;
+	int caracteres = 0;
+	buffer = (char *) malloc (bufsize * sizeof (char));
+	if (buffer == NULL)
+	{
+		exit (1);
+	}
+	printf ("$ ");
+	getline (&buffer, &bufsize, stdin);
+	printf("%d caracteres fueron leídos. \n", caracteres);
+	printf("%s", buffer);
+	free(buffer);
+	return (0);
+}
