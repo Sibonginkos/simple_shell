a90179f6b48a7e8bd1c4f8e8da0aa8be3463fca9
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-06-15 14:16:44 +0200

    Prints $

--- 1-read_getline.c
+++ 1-read_getline.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//int input(char *s,int length);
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
 
-int main()
+int main(void)
 {
 	char *buffer;
 	size_t bufsize = 32;
 	size_t characters;
 
 	buffer = (char *)malloc(bufsize * sizeof(char));
-	if( buffer == NULL)
+	if ( buffer == NULL)
 	{
 		perror("Unable to allocate buffer");
 		exit(1);
 	}
 
 	printf(" $ ");
-	characters = getline(&buffer,&bufsize,stdin);
-	printf("%zu characters were read.\n",characters);
-	printf("You typed: '%s'\n",buffer);
+	characters = getline(&buffer, & bufsize, stdin);
+	printf("%zu characters were read.\n", characters);
+	printf("You typed: '%s'\n", buffer);
 
-	return(0);
+	return (0);
 }
